91261c67a78d029efbb04b180d3e0a90375687d2
ta121443 <ryo_uchi4_20@outlook.jp>
2022-05-28 16:29:06 +0900

    add

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export const TodoList = ({taskList, setTaskList}) => {
+
+    const handleDelete = (id) =>{
+        /*タスクを削除する*/        
+        setTaskList(taskList.filter((task) => task.id !== id));
+    }
+
+    const handleCompleted = (id) => {
+        /*取り消し線の追加*/
+        setTaskList(taskList.map((task) => {
+            if(id === task.id){
+                return {
+                    ...task,
+                    completed: !task.completed
+                };
+            }
+            return task;
+        }))
+    };
+
+    return (
+        <div className='todoList'>
+            <div className='todos'>
+                {taskList.map((task, index) => (
+                    <div className={`todo ${task.completed ? "completed" : ""}`} key={index}>
+                        <div className='todoText'>
+                            <span>{task.text}</span>
+                        </div>
+
+                        <div className='icons'>
+                            <button>
+                                <i className='fa-solid fa-check' onClick={ () => handleCompleted(task.id)}></i>
+                            </button>
+                            <button onClick={ () => handleDelete(task.id)}>
+                                <i className='fa-solid fa-trash'></i>
+                            </button>
+                        </div>
+                    </div>            
+                ))}
+            </div>
+        </div>
+    )
+}
